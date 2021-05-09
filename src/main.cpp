090acb1e9a31d04684ffe09bba0bd4f7dca35b06
Haemavitch Varith <haemavitch@gmail.com>
2021-05-09 12:24:15 +0700

    can read the sensor data

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include <Arduino.h>
-#include <analogWrite.h>
+#include <DHT.h>
 
+// #define DHTTYPE DHT21
 //Buzzer pin
 int BUZZERPIN = 18;
 
+#define DHTTYPE DHT21 // AM2301
+#define DHTPIN 15
+DHT dht(DHTPIN, DHTTYPE);
+
+// DHTNEW mySensor(34);
+
+// uint32_t count = 0;
+// uint32_t start, stop;
+// uint32_t errors[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
 //PWM for the motor driver
-int ENA = 4;
+int ENA = 17;
 int ENB = 16;
 
 // Motor pins
@@ -13,58 +24,85 @@ int IN1A = 27;
 int IN1B = 26;
 int IN2A = 25;
 int IN2B = 33;
+
+//Const pwm values
 const int ledChannel = 0;
 const int freq = 5000;
 const int resolution = 8;
 
 //Sensor pins
+const int sensor = 34;
 
-
-
-
-void setup() {
+void setup()
+{
+  Serial.begin(9600);
+  //Declaring the pins
   pinMode(BUZZERPIN, OUTPUT);
+  //Motor Driver pins
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
   pinMode(IN1A, OUTPUT);
   pinMode(IN1B, OUTPUT);
   pinMode(IN2A, OUTPUT);
   pinMode(IN2B, OUTPUT);
+  // pinMode(sensor, INPUT);
+  dht.begin();
+
+  //Figure out the PWM for both motors
   ledcSetup(ledChannel, freq, resolution);
   ledcAttachPin(ENB, ledChannel);
-  
-
-
-  // put your setup code here, to run once:
+  ledcAttachPin(ENA, ledChannel);
 }
 
-void toggleBuzz(String condition) {
-  if (condition == "off") {
+void toggleBuzz(String condition)
+{
+  if (condition == "off")
+  {
     digitalWrite(BUZZERPIN, LOW);
-  } else {
+  }
+  else
+  {
     digitalWrite(BUZZERPIN, HIGH);
   }
 }
 
-void runFan(int fan, int fanPower0, int fanPower1) {
-  if (fan == 0) {
+void runFan(int fan, int fanPower0, int fanPower1)
+{
+  if (fan == 0)
+  {
     digitalWrite(IN1A, LOW);
     digitalWrite(IN1B, HIGH);
+    ledcWrite(ledChannel, fanPower0);
   }
-  if (fan == 1) {
+  if (fan == 1)
+  {
     digitalWrite(IN2A, LOW);
     digitalWrite(IN2B, HIGH);
-    ledcWrite(ledChannel, fanPower0);
+    ledcWrite(ledChannel, fanPower1);
   }
 }
 
-void loop() {
+void showSensorData()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(t) || isnan(h))
+  {
+    Serial.println("Failed to read from DHT");
+  }
+  else
+ {
+   Serial.print("Humidity: "); 
+   Serial.print(h);
+   Serial.print(" %\t");
+   Serial.print("Temperature: "); 
+   Serial.print(t);
+   Serial.println(" *C");
+   delay(2000);
+ }
+}
 
-  
-  // runFan(1, 100, 0);
-  // digitalWrite(IN2A, LOW);
-  // digitalWrite(IN2B, HIGH);
-  // digitalWrite(IN1A, LOW);
-  // digitalWrite(IN1B, HIGH);
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  showSensorData();
 }
